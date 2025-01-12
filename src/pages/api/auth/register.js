85132c46952b4fc1edacf745feb67864b24d4d51
Anatolii <tolipus@gmail.com>
2025-01-13 00:09:56 +0200

    password encryption and unique email

--- src/pages/api/auth/register.js
+++ src/pages/api/auth/register.js
@@ -1,11 +1,29 @@
-import {supabase} from "../../../../supabase";
 import bcrypt from 'bcrypt';
+import {supabase} from "../../../../supabase";
 
 export default async function handler(req, res) {
     if (req.method === 'POST') {
         const { email, password, name, inviteCode } = req.body;
 
         try {
+
+            // Проверяем, существует ли пользователь с таким email
+            const { data: existingUser, error: existingUserError } = await supabase
+                .from('users')
+                .select('*')
+                .eq('email', email)
+                .single();
+
+            if (existingUser) {
+                return res.status(400).json({ error: 'Email already in use' });
+            }
+
+            if (existingUserError && existingUserError.code !== 'PGRST116') {
+                // Если ошибка не связана с отсутствием строки (PGRST116), возвращаем её
+                return res.status(500).json({ error: existingUserError.message });
+            }
+
+            // Проверяем invite code
             const { data: invite, error: inviteError } = await supabase
                 .from('invite_codes')
                 .select('*')
@@ -17,18 +35,31 @@ export default async function handler(req, res) {
                 return res.status(400).json({ error: 'Invalid or inactive invite code' });
             }
 
+            // Хэшируем пароль
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            // Сохраняем пользователя в базу данных
             const { data: user, error: userError } = await supabase
                 .from('users')
-                .insert([{ email, password, name, invite_code: inviteCode }])
+                .insert([{ email, password: hashedPassword, name, invite_code: inviteCode }])
                 .select('*');
 
             if (userError) {
                 return res.status(500).json({ error: userError.message });
             }
 
+            // Деактивируем invite code
+            await supabase
+                .from('invite_codes')
+                .update({ active: false, used: true })
+                .eq('id', invite.id);
+
+            // Удаляем поле password из ответа
+            const { password: _, ...userWithoutPassword } = user[0];
+
             res.status(201).json({
                 message: 'User registered successfully',
-                user: user[0], // Возвращаем данные первого пользователя
+                user: userWithoutPassword,
             });
         } catch (err) {
             console.error('Unexpected error:', err);
